84e6dc25780f37cfbebaed978ec1b0608966e99d
GrizzlLearn <dylebedev.learning@gmail.com>
2024-08-19 23:51:07 +0300

    2. Сервис - Кинотеатр [#504869]: add main class + packages

---
+++ src/main/java/ru/job4j/cinema/Main.java
@@ -0,0 +1,15 @@
+package ru.job4j.cinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * @author dl
+ * @date 19.08.2024 23:49
+ */
+
+@SpringBootApplication
+public class Main {
+	public static void main(String[] args) {
+		SpringApplication.run(Main.class, args);
+	}
+}
