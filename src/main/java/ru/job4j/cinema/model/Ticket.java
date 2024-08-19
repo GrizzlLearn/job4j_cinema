198c5c3d634c780e272f908a7c8c7bb1934f2034
GrizzlLearn <dylebedev.learning@gmail.com>
2024-08-20 00:03:01 +0300

    2. Сервис - Кинотеатр [#504869]: model ticket done

--- src/main/java/ru/job4j/cinema/model/Ticket.java
+++ src/main/java/ru/job4j/cinema/model/Ticket.java
@@ -1,8 +1,56 @@
 package ru.job4j.cinema.model;
 
+import lombok.Data;
+
+import java.util.Map;
+import java.util.Objects;
+
 /**
  * @author dl
  * @date 19.08.2024 23:51
  */
+
+@Data
 public class Ticket {
+
+	public static final Map<String, String> COLUMN_MAPPING = Map.of(
+			"id", "id",
+			"session_id", "sessionId",
+			"user_id", "userId",
+			"row_number", "rowNumber",
+			"place_umber", "placeNumber"
+	);
+
+	private int id;
+	private int sessionId;
+	private int userId;
+	private int rowNumber;
+	private int placeNumber;
+
+	public Ticket(
+			int id,
+			int sessionId,
+			int userId,
+			int rowNumber,
+			int placeNumber
+			) {
+		this.id = id;
+		this.sessionId = sessionId;
+		this.userId = userId;
+		this.rowNumber = rowNumber;
+		this.placeNumber = placeNumber;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		Ticket ticket = (Ticket) o;
+		return id == ticket.id;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hashCode(id);
+	}
 }
