a94c8814f96eb0149595db0969a6e251dcc97b8e
GrizzlLearn <dylebedev.learning@gmail.com>
2024-08-20 21:43:52 +0300

    2. Сервис - Кинотеатр [#504869]: model Hall done

--- src/main/java/ru/job4j/cinema/model/Hall.java
+++ src/main/java/ru/job4j/cinema/model/Hall.java
@@ -1,8 +1,52 @@
 package ru.job4j.cinema.model;
 
+import lombok.Data;
+
+import java.util.Map;
+import java.util.Objects;
+
 /**
  * @author dl
  * @date 19.08.2024 23:51
  */
+
+@Data
 public class Hall {
+
+	public static final Map<String, String> COLUMN_MAPPING = Map.of(
+			"id", "id",
+			"name", "name",
+			"row_count", "rowCount",
+			"place_count", "placeCount",
+			"description", "description"
+	);
+
+	private int id;
+	private String name;
+	private int rowCount;
+	private int placeCount;
+	private String description;
+
+	public Hall() { }
+
+	public Hall(int id, String name, int rowCount, int placeCount, String description) {
+		this.id = id;
+		this.name = name;
+		this.rowCount = rowCount;
+		this.placeCount = placeCount;
+		this.description = description;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		Hall hall = (Hall) o;
+		return id == hall.id;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hashCode(id);
+	}
 }
